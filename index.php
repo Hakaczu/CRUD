3669a37e74bfde4849c8f07e2dd6cdacbc0ffdbe
Sebastian <32041785+Sisa49@users.noreply.github.com>
2017-12-22 11:07:19 +0100

    Update 0.0.2

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" media="screen" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap.min.css">
+
+	<title>Pharmacy Warehouse</title>
+
+	<!--  -->
+
+	<style>
+
+	</style>
+
+</head>
+
+<body>
+
+	<div class="container">
+		<!-- <div class="navbar navbar-default">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mynavbar-content">
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="">Pharmacy Warehouse</a>
+				</div>
+				<div class="collapse navbar-collapse" id="mynavbar-content">
+					<ul class="nav navbar-nav">
+						<li>
+							<a href="">Add</a>
+						</li>
+						<li>
+							<a href="">View</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div> -->
+		<div class="navbar navbar-default">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mynavbar-content">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Pharmacy Warehouse - Simple CRUD</a>
+				</div>
+				<div class="collapse navbar-collapse" id="mynavbar-content">
+					<ul class="nav navbar-nav">
+						<li>
+							<a href="#">Add</a>
+						</li>
+						<li>
+							<a href="#">View</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+	<script>
+	</script>
+
+</body>
+
+</html>
